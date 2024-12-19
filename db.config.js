6bce443016f4dac38921fe49497fb471f6d5950a
niumengfei <870424431@qq.com>
2024-12-19 18:24:19 +0800

    nodejs

--- db.config.js
+++ db.config.js
@@ -1,20 +1,21 @@
 /*
  * @Author: niumengfei
  * @Date: 2022-10-28 16:47:36
- * @LastEditors: niumengfei 870424431@qq.com
- * @LastEditTime: 2023-01-11 18:07:25
+ * @LastEditors: niumengfei
+ * @LastEditTime: 2023-09-21 11:01:32
  */
 const mongoose = require('mongoose'); //数据库
 require('colors');// 颜色模块
 
 const pk_config = require('./package.json');
 const dbPath = pk_config.runDBPath[process.env.dbPath || 'pro']; //默认连接服务器数据库
-const dbUsername = "app"; // 数据库管理员连接账号
-const dbPassword = "app"; // 数据库管理员的密码
+const dbUsername = "niumengfei"; // 数据库管理员连接账号 app
+const dbPassword = "niumengfei"; // 数据库管理员的密码 app
 
 //启动数据库 插入集合和数据，数据库会自动创建
-// mongoose.connect('mongodb://127.0.0.1:27017/sakuras'); //本地数据库
-mongoose.connect(`mongodb://${dbUsername}:${dbPassword}@${dbPath}`); //通过本地连接远程数据库
+mongoose.connect('mongodb://127.0.0.1:27017/sakuras'); //本地数据库
+// mongoose.connect(`mongodb://${dbUsername}:${dbPassword}@127.0.0.1:27017/sakuras`); //本地数据库
+// mongoose.connect(`mongodb://${dbUsername}:${dbPassword}@${dbPath}`); //通过本地连接远程数据库
 
 const db = mongoose.connection;
 
--- routes/user.js
+++ routes/user.js
@@ -2,105 +2,117 @@
  * @Author: niumengfei
  * @Date: 2022-10-26 18:01:07
  * @LastEditors: niumengfei
- * @LastEditTime: 2023-03-08 09:53:02
+ * @LastEditTime: 2023-09-21 11:02:17
  */
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const UserModel = require('../models/User');
-const DictionaryModel = require('../models/Dictionary')
-const Utils = require('../utils/Utils');
+const UserModel = require("../models/User");
+const DictionaryModel = require("../models/Dictionary");
+const Utils = require("../utils/Utils");
 
 // 校验必填项
-const verifyUser = (req, res) =>{
+const verifyUser = (req, res) => {
     let { sid } = req.headers;
     let { params } = req.body;
     let _sid = JSON.parse(Utils.decrypt.RSA(sid)); // 解密后的sid
     let _pms;
-    try{
+    try {
         _pms = JSON.parse(Utils.decrypt.DynamicDES(params, _sid));
-    }catch(err){
+    } catch (err) {
         res.send({
-            code: '0',
+            code: "0",
             data: null,
-            message: '登录信息验证失败！'
-        })
+            message: "登录信息验证失败！",
+        });
     }
     let { username, password } = _pms;
-    if(!username || !password){
+    if (!username || !password) {
         res.send({
-            code: '0',
+            code: "0",
             data: null,
-            message: '账号和密码不能为空！'
-        })
+            message: "账号和密码不能为空！",
+        });
     }
     return {
         params: _pms,
         sid: _sid,
     };
-}
-
+};
+router.get("/register2", function (req, res, next) {
+    new UserModel({ isAdmin: false, username: "test", password: "5555", age: 15, nickname: "155", emai: "233", adress: "888", school: "000", schedule: "555" }).save((err, newUser) => {
+        if (err) return console.error(err);
+        res.send({
+            code: "1",
+            data: {},
+            message: "注册成功！",
+        });
+    });
+});
 // 注册
-router.post('/register', function(req, res, next) {
+router.post("/register", function (req, res, next) {
     const verify = verifyUser(req, res);
-    if(verify){
-        const { params: { username, password, age, nickname, emai, adress, school, schedule } } = verify;
-        UserModel.findOne({ username })
-        .then(user => {
-            if(!user){
-                new UserModel({ isAdmin: username == 'niumengfei' ? true : false, username, password, age, nickname, emai, adress, school, schedule })
-                .save((err, newUser)=>{
+    if (verify) {
+        const {
+            params: { username, password, age, nickname, emai, adress, school, schedule },
+        } = verify;
+        UserModel.findOne({ username }).then((user) => {
+            if (!user) {
+                new UserModel({ isAdmin: username == "niumengfei" ? true : false, username, password, age, nickname, emai, adress, school, schedule }).save((err, newUser) => {
                     if (err) return console.error(err);
                     res.send({
-                        code: '1',
+                        code: "1",
                         data: {},
-                        message: '注册成功！'
+                        message: "注册成功！",
                     });
                     // 注册成功后应该为用户默认生成文章分类和标签分类
-                    DictionaryModel.insertMany( [
-                        { pid: null, type: 'articleType', label: '文章类型',value: Utils.randomString(8, 'number'), index: 1, userId: newUser._id },
-                        { pid: null, type: 'articleTag', label: '文章标签',value: Utils.randomString(8, 'number'), index: 2, userId: newUser._id }
-                    ]).then(res=>{
-                        
-                    })
-                })
-            }else{
+                    DictionaryModel.insertMany([
+                        { pid: null, type: "articleType", label: "文章类型", value: Utils.randomString(8, "number"), index: 1, userId: newUser._id },
+                        { pid: null, type: "articleTag", label: "文章标签", value: Utils.randomString(8, "number"), index: 2, userId: newUser._id },
+                    ]).then((res) => {});
+                });
+            } else {
                 res.send({
-                    code: '0',
+                    code: "0",
                     data: null,
-                    message: '该账号已存在！'
+                    message: "该账号已存在！",
                 });
             }
-        })
+        });
     }
 });
 
 // 登录
-router.post('/login', function(req, res, next) {
+router.post("/login", function (req, res, next) {
     const verify = verifyUser(req, res);
-    if(verify){
-        const { params: { username, password }, sid } = verify;
-        UserModel.findOne({ username, password })
-        .then(user =>{
-            if(user){
+    if (verify) {
+        const {
+            params: { username, password },
+            sid,
+        } = verify;
+        UserModel.findOne({ username, password }).then((user) => {
+            if (user) {
                 delete user._doc?.password;
-                let encrypt = Utils.encrypt.DynamicDES(JSON.stringify({
-                    ...user._doc,
-                    token: 'eyJ0eXBlIjoiSldUIiwiYWxnIjoiSFMyNTYifQ.eyJpYXQiOjE2NzgyNDAzNTYsInN1YiI6IjkiLCJjZnZPSHk0dEhlTHplLjFhRjRYQ3NyNTdJa1NlV21oVHpZN1NYVVVINlNOY08iLCJjdXN0b21lcklkIjo5LCJ0ZWxlcGhvbmUiOiIxMzkxODkwMTAwMSIsImV4cCI6MTcwOTc3NjM1NiwibmJmIjoxNjc4MjQwMzU2fQ.jbWdYHI1bcIn8OQh98rAlhYANBgi1lUV-I1QibbpWMI'
-                }), sid);
-                console.log('xx', encrypt);
+                let encrypt = Utils.encrypt.DynamicDES(
+                    JSON.stringify({
+                        ...user._doc,
+                        token: "eyJ0eXBlIjoiSldUIiwiYWxnIjoiSFMyNTYifQ.eyJpYXQiOjE2NzgyNDAzNTYsInN1YiI6IjkiLCJjZnZPSHk0dEhlTHplLjFhRjRYQ3NyNTdJa1NlV21oVHpZN1NYVVVINlNOY08iLCJjdXN0b21lcklkIjo5LCJ0ZWxlcGhvbmUiOiIxMzkxODkwMTAwMSIsImV4cCI6MTcwOTc3NjM1NiwibmJmIjoxNjc4MjQwMzU2fQ.jbWdYHI1bcIn8OQh98rAlhYANBgi1lUV-I1QibbpWMI",
+                    }),
+                    sid
+                );
+                console.log("xx", encrypt);
                 res.send({
-                    code: '1',
+                    code: "1",
                     data: encrypt,
-                    message: '登录成功'
-                })
-            }else{
+                    message: "登录成功",
+                });
+            } else {
                 res.send({
-                    code: '0',
+                    code: "0",
                     data: null,
-                    message: '账号名或密码不正确！'
-                })
+                    message: "账号名或密码不正确！",
+                });
             }
-        })
+        });
     }
 });
 
